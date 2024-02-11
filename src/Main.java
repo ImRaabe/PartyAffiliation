ee74d98a5e6098b9db28579fe229ed9e5024a5fd
ImRaabe <raabees@mail.uc.edu>
2024-02-11 15:04:59 -0500

    Project Init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter your party affiliation (D, R, I): ");
+        String party = in.nextLine();
+        if(party.equals("D")) {
+            System.out.println("You get a Democratic Donkey.");
+        }
+            else if(party.equals("R")) {
+                System.out.println("You get a Republican Elephant.");
+            }
+                else if(party.equals("I")) {
+                    System.out.println("You get an Independent Person.");
+                }
+                    else {
+                        System.out.println("You are an 'Other' party.");
+                    }
     }
 }
